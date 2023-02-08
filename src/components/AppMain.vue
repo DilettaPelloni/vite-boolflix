0e0528b2a478ec4cb40e7ae38284c1533110a1f6
Diletta Pelloni <dilpel@live.it>
2023-02-08 15:37:47 +0100

    introdotte immagini

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,6 +16,7 @@
         <h2 v-if="store.filmList.length > 0">FILM</h2>
         <ul>
             <li v-for="film in store.filmList">
+                <img v-if="film.poster_path != null" :src="`https://image.tmdb.org/t/p/w342${film.poster_path}`" :alt="film.title">
                 <p>{{ film.title }}</p>
                 <p>{{ film.original_title }}</p>
                 <p v-if="film.original_language != 'xx'" :class="`fi fi-${film.original_language}`"></p>
@@ -26,6 +27,7 @@
         <h2 v-if="store.seriesList.length > 0">SERIE TV</h2>
         <ul>
             <li v-for="serie in store.seriesList">
+                <img v-if="serie.poster_path != null" :src="`https://image.tmdb.org/t/p/w342${serie.poster_path}`" :alt="serie.name">
                 <p>{{ serie.name }}</p>
                 <p>{{ serie.original_name }}</p>
                 <p v-if="serie.original_language != 'xx'" :class="`fi fi-${serie.original_language}`"></p>
