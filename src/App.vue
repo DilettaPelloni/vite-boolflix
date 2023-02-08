31a299ad8ab536ce0c4ce35252a1e3d1b533cce6
Diletta Pelloni <dilpel@live.it>
2023-02-08 12:51:34 +0100

    creato emit che passa stringa di ricerca

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <script>
+    import axios from 'axios';
+    import {store} from './store'
+    import AppHeader from './components/AppHeader.vue';
     export default {
         name:'App',
-    }
+        components: {
+            AppHeader,
+        },//components
+        data() {
+            return {
+                store,
+            };//return
+        },//data
+        methods: {
+            searchFilm() {
+                // axios.get('', {
+                //     params: {
+                //         api_key: e3bcdf9f6b96589610abc1b9aabec335,
+                //         query: this.store.searchText,
+                //         language: it-IT,
+                //     }
+                // }).then((response) => {
+
+                // });
+                console.log(this.store.searchText)
+            },//searchFilm
+        },//methods
+    };//export
 </script>
 
 <template>
+    <AppHeader @search="searchFilm()"/>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+    import {store} from '../store'
+    export default {
+        name:'AppHeader',
+        data() {
+            return {
+                store,
+            };//return
+        },//data
+    };//export
+</script>
+
+<template>
+    <header>
+        <div class="search-bar">
+            <form @submit.prevent="$emit('search')">
+                <input type="text" v-model="store.searchText">
+                <button>
+                    Cerca
+                </button>
+            </form>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
